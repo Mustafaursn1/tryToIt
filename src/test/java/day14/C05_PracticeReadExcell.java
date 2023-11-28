ac5fc2a54543b96d5a9cbdd7e655dcde7bcf517b
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-11-29 00:09:05 +0100

    add Q read to Excel

---
+++ src/test/java/day14/C05_PracticeReadExcell.java
@@ -0,0 +1,10 @@
+package day14;
+
+public class C05_PracticeReadExcell {
+    /*
+   1-Dosya yolunu bir String degiskene atayalim
+   2 -FileInputStream objesi olusturup, parametre olarak dosya yolunu girelim //-Workbook objesi olusturalim, parameter olarak fileInputstream objesini girelim //-WorkbookFactory.create(fileInputStream)
+   3-sayfa 2'ye gidip satir sayisinin 6, kullanilan satir sayisinin ise 3 oldugunu test edin
+     */
+
+}
