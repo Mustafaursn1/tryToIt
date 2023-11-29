fe2e3061f47c8328ce89ce3de51e822449428dde
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-11-29 22:37:01 +0100

    add delete from Excell

--- src/test/java/day15/C01_DeleteDataExcell.java
+++ src/test/java/day15/C01_DeleteDataExcell.java
@@ -1,21 +1,39 @@
 package day15;
 
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.ss.usermodel.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class C01_DeleteDataExcell {
     @Test
     public void deleteDataFromExcell() throws IOException {
 
-            String filePath = "src/resources/ÜlkelerTest.xlsx";
-            FileInputStream fis = new FileInputStream(filePath);
+        String filePath = "src/resources/ÜlkelerTest.xlsx";
+        FileInputStream fis = new FileInputStream(filePath);
 
-            Workbook workbook = WorkbookFactory.create(fis);
+        Workbook workbook = WorkbookFactory.create(fis);
+        //silecegimiz zaman row objesi ister row olusturmak icin sheet ihtiyaci var
+        //Ayni sekilde cell objesine de ihtiyac duyariz row classinda remoweCell() meth. kullanilir
+        //3.satir 3.cell deki veriyi silelim
+        String silinecekData=workbook.getSheet("Sheet1").getRow(3).getCell(3).toString();
+        Sheet sheet = workbook.getSheet("Sheet1");
+        Row row = sheet.getRow(3);
+        Cell cell =row.getCell(3);
+        row.removeCell(cell);
+        FileOutputStream fos =new FileOutputStream(filePath);
+        workbook.write(fos);
+        //!!!C03_WriteExcell class i ile birlikte degerlendir
+
+        //silindigini test edelim
+        String actualData=null;
+        Assert.assertNotEquals(actualData,silinecekData);
+        System.out.println("SILINEN DATA:"+silinecekData);
 
 
 
@@ -39,14 +57,11 @@ public class C01_DeleteDataExcell {
             String tümTablo = workbook.getSheet("Sheet1").getRow(i).getCell(0).toString() + " ,"
                     + workbook.getSheet("Sheet1").getRow(i).getCell(1).toString() + " ,"
                     + workbook.getSheet("Sheet1").getRow(i).getCell(2).toString() + " ,"
-                    + workbook.getSheet("Sheet1").getRow(i).getCell(3).toString() ;
+                    + workbook.getSheet("Sheet1").getRow(i).getCell(3).toString();
 
 
             System.out.println(tümTablo);
         }
-
-
-
 
 
     }
