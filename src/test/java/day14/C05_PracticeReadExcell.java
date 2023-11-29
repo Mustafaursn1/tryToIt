61ca4f0cec440747205a35bce65cb39bfbf5ca1e
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-11-29 21:18:25 +0100

    add Q Read to Excell

--- src/test/java/day14/C05_PracticeReadExcell.java
+++ src/test/java/day14/C05_PracticeReadExcell.java
@@ -1,10 +1,43 @@
 package day14;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class C05_PracticeReadExcell {
     /*
    1-Dosya yolunu bir String degiskene atayalim
-   2 -FileInputStream objesi olusturup, parametre olarak dosya yolunu girelim //-Workbook objesi olusturalim, parameter olarak fileInputstream objesini girelim //-WorkbookFactory.create(fileInputStream)
-   3-sayfa 2'ye gidip satir sayisinin 6, kullanilan satir sayisinin ise 3 oldugunu test edin
+   2 -FileInputStream objesi olusturup, parametre olarak dosya yolunu girelim
+   3-Workbook objesi olusturalim, parameter olarak fileInputstream objesini girelim
+   4-WorkbookFactory.create(fileInputStream)
+   5-sayfa 2'ye gidip satir sayisinin 6, kullanilan satir sayisinin ise 3 oldugunu test edin
      */
+    @Test
+    public void readExcel() throws IOException {
+        String filePath="src/resources/ÜlkelerTest.xlsx";
+        FileInputStream fis=new FileInputStream(filePath);
+
+        Workbook workbook= WorkbookFactory.create(fis);
+
+        int lastRow = workbook.getSheet("Sheet2").getLastRowNum();
+        System.out.println(lastRow);
+        int ecpextedLastRow=6;
+        //Assert.assertEquals(ecpextedLastRow,lastRow+1);
+        int kullanilanSatirSayisi=workbook.getSheet("Sheet2").getPhysicalNumberOfRows();//Kullanilan satir sayisini aliriz
+        System.out.println("Kullanilan satir sayisi="+kullanilanSatirSayisi);
+        int expectedKullanilanSatir=3;
+        Assert.assertNotEquals(expectedKullanilanSatir,kullanilanSatirSayisi);
+
+
+    }
+
+
+
 
 }
